7bd3d875aa16ddbbea18e9d265c4e6b935ed6bde
邓俊骁 <diagonal@mac-mini.local>
2024-07-04 23:55:20 +0800

    finish task3

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -184,7 +184,10 @@ class Scalar:
         assert h.ctx is not None
 
         # TODO: Implement for Task 1.3.
-        raise NotImplementedError("Need to implement for Task 1.3")
+        cur = h.last_fn.backward(h.ctx, d_output) 
+        for i, input in enumerate(h.inputs):
+            yield input, cur[i]
+        # raise NotImplementedError("Need to implement for Task 1.3")
 
     def backward(self, d_output: Optional[float] = None) -> None:
         """
