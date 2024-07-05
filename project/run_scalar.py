3f21dd05753c19a5f6286b43db9d05a3ec101351
邓俊骁 <diagonal@mac-mini.local>
2024-07-05 23:59:27 +0800

    finish task5

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -7,12 +7,17 @@ import random
 
 import minitorch
 
+from typing import List
+
 
 class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
         # TODO: Implement for Task 1.5.
-        raise NotImplementedError("Need to implement for Task 1.5")
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        self.layer3 = Linear(hidden_layers, 1)
+        # raise NotImplementedError("Need to implement for Task 1.5")
 
     def forward(self, x):
         middle = [h.relu() for h in self.layer1.forward(x)]
@@ -23,8 +28,8 @@ class Network(minitorch.Module):
 class Linear(minitorch.Module):
     def __init__(self, in_size, out_size):
         super().__init__()
-        self.weights = []
-        self.bias = []
+        self.weights: List[List[minitorch.Parameter]] = []
+        self.bias: List[minitorch.Parameter] = []
         for i in range(in_size):
             self.weights.append([])
             for j in range(out_size):
@@ -42,7 +47,14 @@ class Linear(minitorch.Module):
 
     def forward(self, inputs):
         # TODO: Implement for Task 1.5.
-        raise NotImplementedError("Need to implement for Task 1.5")
+        linear_out = []
+        for j in range(len(self.bias)):
+            out = self.bias[j].value
+            for i in range(len(inputs)):
+                out = out + self.weights[i][j].value * inputs[i]
+            linear_out.append(out)
+        return linear_out
+        # raise NotImplementedError("Need to implement for Task 1.5")
 
 
 def default_log_fn(epoch, total_loss, correct, losses):
@@ -105,4 +117,5 @@ if __name__ == "__main__":
     HIDDEN = 2
     RATE = 0.5
     data = minitorch.datasets["Simple"](PTS)
+    print(data)
     ScalarTrain(HIDDEN).train(data, RATE)
